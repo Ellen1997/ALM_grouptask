6de825bc1506155ec7e8ececfc8a30b00dec40ab
Ellen <ellen.holmgren@yh.nackademin.se>
2025-05-30 16:59:58 +0200

    alla ändringar för individuell uppg.

--- src/config/database.js
+++ src/config/database.js
@@ -2,21 +2,29 @@ const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 
 
-dotenv.config();
+dotenv.config();  
 
 let sequelize;
 
 if (process.env.NODE_ENV !== "test") {
-  sequelize = new Sequelize({
-    dialect: "sqlite",
-    storage: process.env.DB_PATH,
-    logging: false, // Set to console.log to see SQL queries
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: "postgres",
+      logging: false, 
+    }
+
+
+  );
 } else {
   sequelize = new Sequelize({
     dialect: "sqlite",
     storage: ":memory:",
-    logging: false, // Set to console.log to see SQL queries
+    logging: false,
   });
 }
 
